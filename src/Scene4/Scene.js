96d59d4df8d6e688aacbe5ce887a70e5501b52b7
Oleg L <o@wemustfly.name>
2018-04-02 22:07:34 +0300

    Scene4 is ready

--- src/Scene4/Scene.js
+++ src/Scene4/Scene.js
@@ -9,14 +9,19 @@ class Scene extends BaseScene {
 
     this.background = background
 
+    const zoom = this.zoom
+    const catsWidth = 1454
+    const catsDelta = 150
     this.pieces.push({
       img: Cats,
-      width: 1454,
+      width: catsWidth,
       left: 645,
       top: 565,
       animation: function (phase) {
         return {
-          opacity: phase === 0 || phase > 2 ? 0 : 1
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          width: (catsWidth - (phase < 2 ? catsDelta : 0)) * zoom,
+          rotateZ: phase > 1 ? 20 : 0
         }
       }
     })
