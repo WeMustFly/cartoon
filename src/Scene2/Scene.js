1922140adb2c7454fa84b297247162b4ef98f2db
Oleg L <o@wemustfly.name>
2018-04-02 16:36:06 +0300

    Scene2 is ready

--- src/Scene2/Scene.js
+++ src/Scene2/Scene.js
@@ -1,5 +1,6 @@
 import BaseScene from '../Scene/Scene'
-import background from './SceneFull.png'
+import background from './Scene.png'
+import Spark from './Spark.png'
 import './Scene.css'
 
 class Scene extends BaseScene {
@@ -7,6 +8,22 @@ class Scene extends BaseScene {
     super(props)
 
     this.background = background
+
+    const zoom = this.zoom
+    this.pieces.push({
+      img: Spark,
+      width: 467,
+      left: 3820,
+      top: 170,
+      animation: function (phase) {
+        return {
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          rotateZ: phase > 1 ? 100 : 0,
+          translateY: phase > 1 ? 2000 * zoom : 0,
+          translateX: phase > 1 ? 1500 * zoom : 0
+        }
+      }
+    })
   }
 }
 
