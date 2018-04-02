495ba95bd04bff1e2ee1c9649f6d6f07a2b0bc13
Oleg L <o@wemustfly.name>
2018-04-02 22:24:51 +0300

    Scene6 is ready

--- src/Scene6/Scene.js
+++ src/Scene6/Scene.js
@@ -10,6 +10,10 @@ class Scene extends BaseScene {
 
     this.background = background
 
+    const zoom = this.zoom
+    const cakeDelta = 300
+    const handsDelta = 200
+
     this.pieces.push({
       img: Cake,
       width: 763,
@@ -17,11 +21,14 @@ class Scene extends BaseScene {
       top: 789,
       animation: function (phase) {
         return {
-          opacity: phase === 0 || phase > 2 ? 0 : 1
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          translateY: phase < 2 ? -1 * (cakeDelta + handsDelta) * zoom : 0,
+          rotateZ: phase < 2 ? 70 : 0
         }
       }
     })
 
+    const handsWidth = 2056
     this.pieces.push({
       img: Hands,
       width: 2056,
@@ -29,7 +36,9 @@ class Scene extends BaseScene {
       top: 0,
       animation: function (phase) {
         return {
-          opacity: phase === 0 || phase > 2 ? 0 : 1
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          translateY: phase < 2 ? -1 * handsDelta * zoom : 0,
+          handsWidth: (phase < 2 ? handsWidth + handsDelta : handsWidth) * zoom
         }
       }
     })
