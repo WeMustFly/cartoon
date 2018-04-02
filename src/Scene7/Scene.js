fe952482cc65c77b714453bedbd3b85189d954b6
Oleg L <o@wemustfly.name>
2018-04-02 22:33:42 +0300

    Scene7 is ready

--- src/Scene7/Scene.js
+++ src/Scene7/Scene.js
@@ -10,26 +10,35 @@ class Scene extends BaseScene {
 
     this.background = background
 
+    const zoom = this.zoom
+    const crowmDelta = 400
+    const crownWidth = 277
     this.pieces.push({
       img: Crown,
-      width: 277,
+      width: crownWidth,
       left: 2098,
       top: 2269,
       animation: function (phase) {
         return {
-          opacity: phase === 0 || phase > 2 ? 0 : 1
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          translateY: phase < 2 ? crowmDelta * zoom : 0,
+          width: (phase < 2 ? (crownWidth + crowmDelta / 5) : crownWidth) * zoom
         }
       }
     })
 
+    const shoesDelta = 600
+    const shoesWidth = 469
     this.pieces.push({
       img: Shoes,
-      width: 469,
+      width: shoesWidth,
       left: 161,
       top: 107,
       animation: function (phase) {
         return {
-          opacity: phase === 0 || phase > 2 ? 0 : 1
+          opacity: phase === 0 || phase > 2 ? 0 : 1,
+          translateY: phase < 2 ? -1 * shoesDelta * zoom : 0,
+          width: (phase < 2 ? (shoesWidth + shoesDelta / 5) : shoesWidth) * zoom
         }
       }
     })
